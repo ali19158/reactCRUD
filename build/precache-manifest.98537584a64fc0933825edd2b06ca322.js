295565be9f74232538d18635fb0cda4c16a40f93
ali19158 <ali19158@gmail.com>
2019-09-24 23:46:32 +0600

    crud

---
+++ build/precache-manifest.98537584a64fc0933825edd2b06ca322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beccd532421b2a4f1be3f28fd24609f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9240f9a8e8b981847865",
+    "url": "/static/css/main.a371a94d.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "9240f9a8e8b981847865",
+    "url": "/static/js/main.a9e78bbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
